5171da7db8ef6f6fa75ae79cc99eb4568f16bf63
ng0035 <nikitagupta0809@gmail.com>
2020-10-30 15:23:14 -0400

    initialized react app

---
+++ dashboard/src/App.js
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">
+          {/* <img src="/Users/nikitagupta/Desktop/Dashboard/dashboard/public/logo.png" alt="company logo"/> */}
+          StackLine
+        </a>
+      </header>
+      <main>
+        Chart
+      </main>
+    </div>
+
+  );
+}
+
+export default App;
