ce2c93b34601c7a7ad6d9f9a23c1160c00b0b54b
ng0035 <nikitagupta0809@gmail.com>
2020-11-01 16:13:14 -0500

    adding redux

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,18 +1,36 @@
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">
-          {/* <img src="/Users/nikitagupta/Desktop/Dashboard/dashboard/public/logo.png" alt="company logo"/> */}
-          StackLine
-        </a>
-      </header>
-      <main>
-        Chart
-      </main>
-    </div>
+import React from 'react';
+import Canvas from "./components/Canvas";
+import SideBar from "./components/SideBar";
+import data from "./components/Webdev_data2.json";
 
-  );
-}
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      products: data.products
+    };
+  }
+  render(){
 
+    
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">
+            <img src="/logo.png" alt="company-logo" className="brand-logo"/> 
+          </a>
+        </header>
+        <main>
+          <div className="sidebar">
+            <SideBar products={this.state.products}/>
+          </div>
+          <div className="canvas">
+            <Canvas products={this.state.products}/>
+          </div>
+        </main>
+      </div>
+
+    );
+    }
+  }
 export default App;
---
+++ dashboard/src/components/SideBar.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+export default class SideBar extends Component {
+    render() {
+        return (
+            <div>
+                    {this.props.products.map((product)=>(
+                    <div>
+                        <div className="prod-intro">
+                            <img src={product.image} alt={product.title} className="prod-img"></img>
+                            <p><b>{product.title}</b></p>
+                            <p className="subtitle">{product.subtitle}</p>
+                        </div>
+                        <ul className="tags">
+                            {product.tags.map(item => 
+                                <li key={item}>
+                                    <button className="button button-primary">{item}</button>
+                                </li>)}
+                        </ul>
+                        <div className="tabs">
+                            <button className="tab">
+                                <img src="/home.svg" height="20px" width="20px" alt="home icon" className="icon"></img>  
+                                    OVERVIEW</button>
+                            <button className="tab active">
+                            <img src="/sales.svg" height="20px" width="20px" alt="sales icon" className="icon"></img>   
+                                    SALES</button>
+                        </div>  
+                    </div>
+                    ))}
+            </div>
+        )
+    }
+}
